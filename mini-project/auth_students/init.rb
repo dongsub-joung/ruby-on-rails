ad46295e1a184d1aa26878367644c480f981f117
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-03-08 05:22:59 +0900

    ing

--- mini-project/auth_students/init.rb
+++ mini-project/auth_students/init.rb
@@ -14,8 +14,20 @@ class Student
 	end
 end
 
+
+dongsub_id= "dongsub"
+dongsub_pw= "123"
+dongsub= Student.new(dongsub_id, dongsub_pw)
+db= { "dongsub" => dongsub }
+
 inputs= gets.chomp.split(" ")
 id= inputs[0]
 pw= inputs[1]
-dongsub= Student.new(id, pw)
-dongsub.to_s
+
+if db.include?(id)
+	p db[id]
+else 
+	someone= Student.new(id, pw)
+	db[id]= someone
+	someone.to_s
+end
