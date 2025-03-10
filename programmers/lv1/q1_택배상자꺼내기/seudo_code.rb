8e98c726ddb4f5b4b488e98a9590709808aa135e
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2025-03-10 19:58:45 +0900

    done

---
+++ programmers/lv1/q1_택배상자꺼내기/seudo_code.rb
@@ -0,0 +1,20 @@
+def solution(n, w, num)
+    answer = 0
+
+    store= Vector.new
+    for i in 0..w
+        store[store.zero(w)]
+    end
+
+    position= n % w
+
+    # 1 ≤ w ≤ 10
+    if position == 0
+        # deep
+    elsif position == 1
+
+#   elsif .. position == 10
+    end
+
+    return answer
+end
